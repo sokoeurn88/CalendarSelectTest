ad462d656cf5e6552af1ee3558688271fa8179dc
sokoeurn88 <92101403+sokoeurn88@users.noreply.github.com>
2022-09-03 09:10:36 -0500

    calendarselecttest

---
+++ src/calendarselecttest/CalentarSelectTest.java
@@ -0,0 +1,12 @@
+package calendarselecttest;
+
+public class CalentarSelectTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
---
+++ src/calendarselecttest/WebDriver.java
@@ -0,0 +1,5 @@
+package calendarselecttest;
+
+public class WebDriver {
+
+}
